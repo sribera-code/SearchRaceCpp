8221098812fa31f7b6fed5d329c1c95c25827c30
sribera-code <ribera.sebastien@gmail.com>
2020-09-07 01:01:44 +0200

    2020-09-07

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,10 @@
 
+const auto testGame = true;
+const auto withRandomTests = true;
+
 #include <algorithm>
+#include <array>
+#include <cassert>
 #include <chrono>
 #include <cmath>
 #include <complex>
@@ -7,6 +12,8 @@
 #include <iomanip>
 #include <iostream>
 #include <iterator>
+#include <limits>
+#include <cmath> 
 #include <numeric>
 #include <random>
 #include <sstream>
@@ -14,10 +21,58 @@
 #include <vector>
 
 using namespace std::literals::complex_literals;
-using Z = std::complex<double>;
+
+using Distance = double;
+using Norm = double;
+using Z = std::complex<Distance>;
 using Angle = int;
 using Thrust = unsigned;
-const double Pi = 3.141592653589793238463;
+using Step = std::size_t;
+using Milliseconds = unsigned;
+
+const double pi = 3.141592653589793238463;
+const double radByDeg = pi / 180.;
+const double degByRad = 180. / pi;
+const Distance epsilon = .00001;
+const double degEpsilon = .1;
+const Angle angleMax = 18;
+const Thrust thrustMax = 200;
+const Distance xMax = 16000, yMax = 9000, checkpointRadius = 600;
+const double checkpointRadiusSquare = checkpointRadius * checkpointRadius;
+const double friction = 0.15;
+const Milliseconds stepTime = 40;
+const unsigned iterationsLimit = 600;
+
+template <typename T>
+static T getInfinity()
+{
+	return std::numeric_limits<T>::max();
+}
+
+template <typename Destination, typename Source>
+static void transfer(Destination& destination, Source&& source)
+{
+	destination = source;
+}
+
+template <typename Destination, typename Source, typename... Args>
+static void transfer(Destination& destination, Source&& source, Args&&...args)
+{
+	destination = source;
+	transfer(std::forward<Args>(args)...);
+}
+
+using TimePoint = std::chrono::high_resolution_clock::time_point;
+
+static TimePoint now()
+{
+	return std::chrono::high_resolution_clock::now();
+}
+
+static Milliseconds getMillisecondsElapsed(TimePoint const& start, TimePoint const& end)
+{
+	return static_cast<Milliseconds>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+}
 
 struct IO
 {
@@ -62,26 +117,104 @@ Z IO::read<Z>(bool end)
 	return { read<double>(), read<double>(end) };
 }
 
-struct Test
+static const unsigned getSeed()
 {
-	Test()
-		: m_io({ m_in, std::cerr, m_out })
-	{}
+	//const auto seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
+	const auto seed = 0u;
+	std::cerr << "seed=" << seed << std::endl;
+	return seed;
+}
 
-	std::istringstream m_in;
-	//std::ostringstream m_err;
-	std::ostringstream m_out;
-	IO m_io;
-};
+static const unsigned seed()
+{
+	//static const auto seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
+	static const auto seed = getSeed();
+	return seed;
+}
 
+template<typename T, T min, T max>
+static T getRandom()
+{
+	static std::default_random_engine generator(seed());
+	//static std::default_random_engine generator(0u);
+	static std::uniform_int_distribution<T> distribution(min, max);
+	return distribution(generator);
+}
+
+static auto getRandomAngle = getRandom<Angle, -angleMax, +angleMax>;
+static auto getRandomThrust = getRandom<Thrust, 0, thrustMax>;
+
+template<typename T, T tMin, T tMax>
+static T getAngle(T t)
+{
+	if (t < tMin) return getAngle<T, tMin, tMax>(t + tMax - tMin);
+	if (t < tMax) return t;
+	return getAngle<T, tMin, tMax>(t - tMax + tMin);
+}
+
+template<typename T>
+static T get360Angle(T t)
+{
+	return getAngle<T, 0, 360>(t);
+}
+
+template<typename T>
+static T get180Angle(T t)
+{
+	return getAngle<T, -180, +180>(t);
+}
+
+template<typename T, T tMin, T tMax>
+static T getValid(T t)
+{
+	if (t < tMin) return tMin;
+	if (t > tMax) return tMax;
+	return t;
+}
+
+template<typename T, T tMin, T tMax>
+static bool isValid(T t)
+{
+	return tMin <= t && t <= tMax;
+}
+
+static auto getValidAngle = getValid<Angle, -angleMax, +angleMax>;
+static auto getValidThrust = getValid<Thrust, 0, thrustMax>;
+
+static auto isValidAngle = isValid<Angle, -angleMax, +angleMax>;
+static auto isValidThrust = isValid<Thrust, 0, thrustMax>;
+
+static std::array<Z, 360> getPolarArray()
+{
+	std::array<Z, 360> polarArray;
+	for (Angle angle = 0; angle < 360; ++angle)
+		polarArray[angle] = std::polar(1., radByDeg * angle);
+	return polarArray;
+}
+
+static Z getPolar(Angle angle)
+{
+	static auto const polarArray = getPolarArray();
+	return polarArray[get360Angle(angle)];
+}
+
+static Z truncateZ(Z z)
+{
+	return { std::trunc(z.real() + copysign(epsilon, z.real())), std::trunc(z.imag() + copysign(epsilon, z.imag())) };
+}
 
 struct Game
 {
 	static Game read(IO& io)
 	{
 		Game g;
-		std::generate_n(std::back_inserter(g.m_checkpoints), io.read<std::size_t>(true), [&io]() { return io.read<Z>(true); });
+		std::generate_n(std::back_inserter(g.m_checkpoints), io.read<Step>(true), [&io]() { return io.read<Z>(true); });
 		return g;
+	}
+
+	Z getCheckpoint(Step step) const
+	{
+		return m_checkpoints[step];
 	}
 
 	std::vector<Z> m_checkpoints;
@@ -103,41 +236,200 @@ static std::ostream& operator<<(std::ostream& os, Game const& g)
 
 struct State
 {
-	std::size_t m_step = {};
+	State() = default;
+	State(Step step, Z position, Z speed, Angle angle)
+		: m_step(step), m_position(std::move(position)), m_speed(std::move(speed)), m_angle(get360Angle(angle))
+	{}
+
+	Step m_step = {};
 	Z m_position, m_speed;
 	Angle m_angle = {};
 
 	static State read(IO& io)
 	{
-		return { io.read<std::size_t>(), io.read<Z>(), io.read<Z>(), io.read<Angle>(true) };
+		return { io.read<Step>(), io.read<Z>(), io.read<Z>(), io.read<Angle>(true) };
 	}
 };
 
-static std::ostream& operator<<(std::ostream& os, State const& s)
+static bool operator==(State const& lhs, State const& rhs)
 {
-	return os << std::fixed << std::setprecision(0) << "State: step=" << s.m_step << " position=" << s.m_position << " speed=" << s.m_speed << " angle=" << s.m_angle;
+	std::cerr << (lhs.m_step == rhs.m_step) << (lhs.m_angle == rhs.m_angle)
+		<< (std::norm(lhs.m_position - rhs.m_position) < epsilon)
+		<< (std::norm(lhs.m_speed - rhs.m_speed) < epsilon) << std::endl;
+	return lhs.m_step == rhs.m_step && lhs.m_angle == rhs.m_angle
+		&& std::norm(lhs.m_position - rhs.m_position) < epsilon
+		&& std::norm(lhs.m_speed - rhs.m_speed) < epsilon;
 }
 
-const Angle angleMax = 18;
-const Thrust thrustMax = 200;
-
-static void runGame()
+static std::ostream& operator<<(std::ostream& os, State const& s)
 {
-	auto io = IO::make();
-	auto g = Game::read(io);
-	io.m_err << io.getLastRead() << std::endl;
-	io.m_err << g << std::endl;
-	auto seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
-	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<Angle> distributionAngle(-angleMax, +angleMax);
-	std::uniform_int_distribution<Thrust> distributionThrust(0, +thrustMax);
+	return os << std::fixed << std::setprecision(0) << "State: step=" << s.m_step
+		<< " position=" << s.m_position << "[" << std::abs(s.m_position) << "," << std::arg(s.m_position) * degByRad << "deg]"
+		<< " speed=" << s.m_speed << "[" << std::abs(s.m_speed) << "," << std::arg(s.m_speed) * degByRad << "deg] angle=" << s.m_angle << "deg";
+}
+
+struct Command
+{
+	Command() = default;
+	Command(Angle angle, Thrust thrust) : m_angle(angle), m_thrust(thrust) {}
+
+	static Command makeValidCommand(Angle angle, Thrust thrust)
+	{
+		return { getValidAngle(get180Angle(angle)), getValidThrust(thrust) };
+	}
+
+	Angle m_angle = {};
+	Thrust m_thrust = {};
+
+	static Command getRandom()
+	{
+		return { getRandomAngle(), getRandomThrust() };
+	}
+
+	State move(State state) const
+	{
+		state.m_angle = get360Angle(state.m_angle + m_angle);
+		state.m_speed += static_cast<Z::value_type>(m_thrust) * getPolar(state.m_angle);
+		state.m_position += state.m_speed;
+		state.m_speed *= 1. - friction;
+		return state;
+	}
+
+	State move(Game const& game, State state) const
+	{
+		auto oldPosition = state.m_position;
+		state = move(std::move(state));
+		auto newPosition = state.m_position;
+		auto checkpoint = game.getCheckpoint(state.m_step);
+		if (std::norm(checkpoint - newPosition) <= checkpointRadiusSquare)
+			++state.m_step;
+		else
+		{
+			auto deltaPosition = newPosition - oldPosition;
+			auto distance = std::abs(deltaPosition);
+			auto product = deltaPosition * std::conj(checkpoint - oldPosition) / distance;
+			if (-checkpointRadius <= product.imag() && product.imag() <= +checkpointRadius)
+				if (0 <= product.real() && product.real() <= distance)
+					++state.m_step;
+		}
+		state.m_position = truncateZ(state.m_position);
+		state.m_speed = truncateZ(state.m_speed);
+		return state;
+	}
+};
+
+static Command getDirectCommand(Game const& game, State const& currentState)
+{
+	auto computeStatus = [&game](State const& state) { return std::make_tuple(-static_cast<int>(state.m_step), std::norm(game.getCheckpoint(state.m_step) - state.m_position)); };
+	auto bestStatus = std::make_tuple(0, getInfinity<Norm>());
+	Command bestCommand;
+	for (Angle angle = -angleMax; angle <= angleMax; ++angle)
+	{
+		Command command(angle, thrustMax);
+		auto newState = command.move(game, currentState);
+		auto newStatus = computeStatus(newState);
+		if (newStatus < bestStatus)
+			transfer(bestStatus, newStatus, bestCommand, command);
+	}
+	Command commandWithoutThrust(bestCommand.m_angle, 0);
+	auto newStateWithoutThrust = commandWithoutThrust.move(game, currentState);
+	auto newStatusWithoutThrust = computeStatus(newStateWithoutThrust);
+	if (newStatusWithoutThrust <= bestStatus)
+		return commandWithoutThrust;
+	auto currentStatus = computeStatus(currentState);
+	if (currentStatus <= bestStatus)
+		return commandWithoutThrust;
+	return bestCommand;
+
+	//auto target = game.getCheckpoint(currentState.m_step);
+	//auto distanceToTarget = std::abs(positionToTarget);
+	//auto angleRadius = std::atan2(checkpointRadius, distanceToTarget);
+	//auto angleSpeed = std::arg(state.m_speed) * degByRad;
+	//auto angleToTarget = std::arg(positionToTarget) * degByRad;
+	//auto commandAngle = get180Angle(static_cast<Angle>(std::round(angleToTarget - state.m_angle)));
+	//if (isValidAngle(commandAngle))
+	//	return Command(commandAngle, thrustMax);
+	//return Command(getValidAngle(commandAngle), 0);
+}
+
+static std::ostream& operator<<(std::ostream& os, Command const& c)
+{
+	return os << "EXPERT " << c.m_angle << " " << c.m_thrust << std::endl;
+}
+
+static unsigned reachNext(Game const& game, TimePoint const& start, unsigned timeLimit, Step initialStep, State state)
+{
+	auto step = std::min(initialStep + 2, game.m_checkpoints.size() - 1);
+	unsigned time = 0;
+	while (state.m_step < step)
+	{
+		if (++time >= timeLimit)
+			return getInfinity<unsigned>();
+		auto command = getDirectCommand(game, state);
+		state = command.move(game, state);
+		if (getMillisecondsElapsed(start, now()) >= stepTime)
+			return getInfinity<unsigned>();
+	}
+	return time;
+}
+
+static unsigned runGame(IO& io, Game const& game)
+{
+	State lastState;
+	unsigned totalTestsCount = 0, totalImprovementsCount = 0, iterationsCount = 0;
 	while (true)
 	{
-		auto s = State::read(io);
+		auto currentState = State::read(io);
 		io.m_err << io.getLastRead() << std::endl;
-		io.m_err << s << std::endl;
-		io.m_out << "EXPERT " << distributionAngle(generator) << " " << distributionThrust(generator) << std::endl;;
+		io.m_err << "old: " << currentState << std::endl;
+		if (!iterationsCount)
+			io.m_err << io.getLastRead() << std::endl;
+		else if (testGame)
+			assert(lastState == currentState);
+		++iterationsCount;
+
+		auto bestCommand = getDirectCommand(game, currentState);
+		auto bestState = bestCommand.move(game, currentState);
+		auto start = now();
+		auto bestTime = withRandomTests ? reachNext(game, start, getInfinity<unsigned>(), currentState.m_step, bestState) : 0;
+		unsigned testsCount = 0;
+		if (withRandomTests)
+			while (getMillisecondsElapsed(start, now()) < stepTime)
+			{
+				++testsCount;
+				auto command = Command::getRandom();
+				auto state = command.move(game, currentState);
+				auto time = reachNext(game, start, bestTime, currentState.m_step, state);
+				if (time < bestTime)
+				{
+					transfer(bestTime, time, bestCommand, command, bestState, state);
+					++totalImprovementsCount;
+					io.m_err << "best: time=" << bestTime << " state=" << bestState << std::endl;
+				}
+			}
+		bestState = bestCommand.move(game, currentState);
+		io.m_err << "elapsed=" << getMillisecondsElapsed(start, now()) << "ms" << std::endl;
+		io.m_err << "testsCount=" << testsCount << " totalImprovements=" << totalImprovementsCount << std::endl;
+		totalTestsCount += testsCount;
+		io.m_err << "best: time=" << bestTime << " - " << bestState << std::endl;
+		bool endGame = bestState.m_step == game.m_checkpoints.size() || iterationsCount == iterationsLimit;
+		if (endGame)
+			io.m_err << "totalTestsCount=" << totalTestsCount << " iterationsCount=" << iterationsCount << std::endl;
+		lastState = bestState;
+		io.m_out << bestCommand;
+		if (endGame)
+			break;
 	}
+	return iterationsCount;
+}
+
+static unsigned runGame()
+{
+	auto io = IO::make();
+	const auto game = Game::read(io);
+	io.m_err << io.getLastRead() << std::endl;
+	io.m_err << game << std::endl;
+	return runGame(io, game);
 }
 
 #ifndef TESTS
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -12,6 +12,13 @@ static std::string replaceSub(std::string const& str, std::string const& sub, st
 	return res;
 }
 
+template<typename T>
+static std::string toString(T&& t)
+{
+	std::ostringstream s; s << t;
+	return s.str();
+}
+
 struct SearchRaceTest : public ::testing::Test
 {
 	SearchRaceTest() : m_io({ m_in, std::cerr, m_out }) {}
@@ -25,7 +32,71 @@ TEST_F(SearchRaceTest, ReadGameInput)
 {
 	std::string gameInput = "9 \n2757 4659 \n3358 2838 \n10353 1986 \n2757 4659 \n3358 2838 \n10353 1986 \n2757 4659 \n3358 2838 \n10353 1986 \n";
 	m_in.str(gameInput);
-	auto g = Game::read(m_io);
-	m_io.m_err << g << std::endl;
+	auto game = Game::read(m_io);
 	EXPECT_EQ(replaceSub(m_io.getLastRead(), "\\\\n", "\n"), gameInput);
+	EXPECT_EQ(game.m_checkpoints.size(), 9);
+	EXPECT_EQ(game.getCheckpoint(3), Z(2757, 4659));
+	std::string stateInput = "0 10353 1986 0 0 161 \n";
+	m_in.str(stateInput);
+	auto state = State::read(m_io);
+	EXPECT_EQ(replaceSub(m_io.getLastRead(), "\\\\n", "\n"), stateInput);
+	EXPECT_EQ(state.m_step, 0);
+	EXPECT_EQ(state.m_position, Z(10353, 1986));
+	EXPECT_EQ(state.m_speed, Z(0, 0));
+	EXPECT_EQ(state.m_angle, 161);
+}
+
+TEST_F(SearchRaceTest, WriteCommand)
+{
+	EXPECT_EQ(toString(Command{ angleMax, thrustMax }), "EXPERT 18 200\n");
+	EXPECT_EQ(toString(Command()), "EXPERT 0 0\n");
+}
+
+TEST_F(SearchRaceTest, RandomCommand)
+{
+	for (unsigned i = 0; i < 100; ++i)
+	{
+		auto c = Command::getRandom();
+		EXPECT_LE(std::abs(c.m_angle), angleMax);
+		EXPECT_LE(c.m_thrust, thrustMax);
+	}
+}
+
+TEST_F(SearchRaceTest, Polar)
+{
+	EXPECT_LE(std::abs(getPolar(- 90) - (-1.i)), epsilon);
+	EXPECT_LE(std::abs(getPolar(   0) - (+1. )), epsilon);
+	EXPECT_LE(std::abs(getPolar(+ 90) - (+1.i)), epsilon);
+	EXPECT_LE(std::abs(getPolar(+180) - (-1. )), epsilon);
+	EXPECT_LE(std::abs(getPolar(+270) - (-1.i)), epsilon);
+	EXPECT_LE(std::abs(getPolar(+360) - (+1. )), epsilon);
+	EXPECT_LE(std::abs(getPolar(+450) - (+1.i)), epsilon);
+}
+
+TEST_F(SearchRaceTest, State)
+{
+	State state;
+	state.m_angle = 140;
+	Command command(10, 100);
+	auto newState = command.move(state);
+	EXPECT_EQ(state.m_angle + command.m_angle, newState.m_angle);
+	EXPECT_NEAR(std::arg(newState.m_speed) * degByRad, newState.m_angle, degEpsilon);
+}
+
+TEST_F(SearchRaceTest, Transfer)
+{
+	unsigned u = 3u;
+	std::string s = "3";
+	double d = -3.;
+	transfer(u, 2u, s, "2", d, -2.);
+	EXPECT_EQ(u, 2u);
+	EXPECT_EQ(s, "2");
+	EXPECT_EQ(d, -2.);
+}
+
+TEST_F(SearchRaceTest, Simulations)
+{
+	std::string gameInput = "9 \n2757 4659 \n3358 2838 \n10353 1986 \n2757 4659 \n3358 2838 \n10353 1986 \n2757 4659 \n3358 2838 \n10353 1986 \n";
+	m_in.str(gameInput);
+	auto game = Game::read(m_io);
 }
